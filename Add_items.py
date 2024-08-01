f7e69762cd270527d118acd12506259af53b444b
HARSHIT-SAXENA3007 <149141289+HARSHIT-SAXENA3007@users.noreply.github.com>
2024-08-01 11:42:37 +0530

    Create Add_items.py

---
+++ Add_items.py
@@ -0,0 +1,11 @@
+def add_item_to_database():
+    item_name = simpledialog.askstring("Add Item", "Enter the item name:")
+    if item_name:
+        item_price = simpledialog.askfloat("Add Item", "Enter the item price:")
+        if item_price:
+            try:
+                cursor.execute("INSERT INTO items (item_name, price) VALUES (%s, %s)", (item_name, item_price))
+                db.commit()
+                speak(f"Item '{item_name}' with price {item_price} has been added to the database.")
+            except mysql.connector.Error as error:
+                speak(f"Error adding item to the database: {error}")
