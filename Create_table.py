43cf0c7fefb0966dd4b2f96aec4567afa6df53fe
HARSHIT-SAXENA3007 <149141289+HARSHIT-SAXENA3007@users.noreply.github.com>
2024-08-01 11:13:24 +0530

    Create Create_table.py

---
+++ Create_table.py
@@ -0,0 +1,10 @@
+ table = Table(data)
+    style = TableStyle([('BACKGROUND', (0,0), (-1,0), colors.grey),
+                        ('TEXTCOLOR', (0,0), (-1,0), colors.whitesmoke),
+                        ('ALIGN', (0,0), (-1,-1), 'CENTER'),
+                        ('FONTNAME', (0,0), (-1,0), 'Helvetica-Bold'),
+                        ('BOTTOMPADDING', (0,0), (-1,0), 12),
+                        ('BACKGROUND', (0,1), (-1,-1), colors.beige),
+                        ('GRID', (0,0), (-1,-1), 1, colors.black)])
+    table.setStyle(style)
+    elements.append(table)
