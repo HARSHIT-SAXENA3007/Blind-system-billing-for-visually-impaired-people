96bee8c0dccd1df403b3d6b825cb77954431f265
HARSHIT-SAXENA3007 <149141289+HARSHIT-SAXENA3007@users.noreply.github.com>
2024-08-01 11:09:23 +0530

    Create Generate_bill.py

---
+++ Generate_bill.py
@@ -0,0 +1,7 @@
+def generate_pdf_bill(order_items, total_price):
+    # Create a list of data for the table
+    data = [["Item", "Quantity", "Price"]]
+    for item_name, quantity, price in order_items:
+        data.append([item_name, str(quantity), str(price)])
+    data.append(["", "", ""])
+    data.append(["Total", "", str(total_price)])
