b8453b270b55662059c2c20baeb1087dbe162dea
HARSHIT-SAXENA3007 <149141289+HARSHIT-SAXENA3007@users.noreply.github.com>
2024-08-01 11:07:25 +0530

    Create Listen_to_voice_input.py

---
+++ Listen_to_voice_input.py
@@ -0,0 +1,14 @@
+def listen():
+    with sr.Microphone() as source:
+        status_label.configure(text="Listening...")
+        speak("speak now")
+        audio = recognizer.listen(source)
+        try:
+            command = recognizer.recognize_google(audio)
+            status_label.configure(text=f"User said: {command}")
+            print(f"User said: {command}")
+            return command
+        except sr.UnknownValueError:
+            status_label.configure(text="Sorry, I did not understand that.")
+            speak("Sorry, I did not understand that.")
+            return None
